ee0dc91cf017249ad81c84f572b344a6139f4fef
tomer1983 <22876011+tomer1983@users.noreply.github.com>
2023-04-24 14:09:14 +0300

    Update build.sh

---
+++ my-custom-python-exporter/build.sh
@@ -0,0 +1,50 @@
+#!bash
+set -e
+PROJECT_NAME='sre-hcp-exporter'
+PROJECT_TAG='v0.1'
+RANDOM=$(echo $RANDOM)
+
+
+# mkdir -p code/dependencies
+# pip3 download -r code/requirements.txt -d "code/dependencies" 
+# tar cvfz code/dependencies.tar.gz code/dependencies
+
+
+
+echo -e "PROJECT: $PROJECT_NAME:$PROJECT_TAG hash:$RANDOM"
+
+# #login to artifactory
+#docker login -u <user> <repor_url> -p <pass>
+
+# #build the image
+docker build --force-rm -t $PROJECT_NAME:$PROJECT_TAG code/.
+
+
+# # This command tags the Docker image with the given project name and tag, and specifies the destination registry and repository where the image should be stored.
+docker tag $PROJECT_NAME:$PROJECT_TAG <repor_url>/sre-tools/$PROJECT_NAME:$PROJECT_TAG
+
+# # This command pushes the Docker image to the specified destination registry and repository.
+docker push <repor_url>/sre-tools/$PROJECT_NAME:$PROJECT_TAG
+
+# # This command displays a message to indicate that the image has been successfully pushed to the destination repository and registry.
+echo -e "DONE - image pushed to <repor_url>/sre-tools/$PROJECT_NAME:$PROJECT_TAG"
+
+# # building a kuberntes.yaml file with the right arguments
+sed "s/__PROJECT_NAME__/$PROJECT_NAME/g" manifest/kubernetes.templete.yaml > manifest/kubernetes.yaml
+sed -i "s/__PROJECT_TAG__/$PROJECT_TAG/g" manifest/kubernetes.yaml
+sed -i "s/__PROJECT_TAG__/$PROJECT_TAG/g" manifest/cm.yaml
+
+
+# Redeploy
+oc delete -f ./manifest/kubernetes.yaml --force --grace-period=0 2>/dev/null || true
+oc delete -f ./manifest/cm.yaml --force --grace-period=0 2>/dev/null || true
+sleep 5
+oc apply -f ./manifest/cm.yaml 
+oc apply -f ./manifest/kubernetes.yaml 
+sleep 5
+while [ -z "$(oc get pods --sort-by=.metadata.creationTimestamp -o name | grep $PROJECT_NAME |  tail -1 | cut -d/ -f2)" ]; do echo waiting for pod to start & sleep 1 ; done 
+POD=$( oc get pods --sort-by=.metadata.creationTimestamp -o name | grep $PROJECT_NAME |  tail -1 | cut -d/ -f2)
+echo "#POD : $POD"
+ROUTE=$(oc get route | grep sre)
+echo "ROUTE IS : $ROUTE"
+oc wait --for=condition=Ready pod $POD && oc logs -f $POD
